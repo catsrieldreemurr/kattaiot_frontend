bed091e41ca306aa7e0ff9015b562bff969e0a23
StargazerCatsriel <danobrovold@gmail.com>
2026-03-10 10:30:15 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,6 @@ import { Button } from "@/components/ui/button"
 
 export default function Page() {
   return (
-          <Button className="mt-2">Button</Button>
-        </div>
     <div>
       <p>test</p>
     </div>
